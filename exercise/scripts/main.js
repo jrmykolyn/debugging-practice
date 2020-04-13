619367165f71fee866e9b0144e15a8fa281b43b5
khush1611 <khushboo.umrigar11@gmail.com>
2020-04-13 19:06:45 -0400

    Cards do not seem to be compared/matched correctly has been fixed

--- exercise/scripts/main.js
+++ exercise/scripts/main.js
@@ -20,7 +20,7 @@ window.addEventListener('DOMContentLoaded', function() {
       // If we have selected two cards, see if they match.
       if (selectedCards.length === 2) {
         var card1 = selectedCards[0];
-        var card2 = selectedCards[0];
+        var card2 = selectedCards[1];
 
         // If the cards match, add them to the collection of matched cards and
         // apply the correct CSS class.
